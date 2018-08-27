17466cd821d637106e92ce1762fa38c3647dc3e7
Sav Younts <savyounts@gmail.com>
2018-08-27 20:19:29 +0000

    automatically backed up by learn

--- db/migrate/008_add_character_to_actor.rb
+++ db/migrate/008_add_character_to_actor.rb
@@ -1,5 +1,5 @@
 class AddCharacterToActor < ActiveRecord::Migration[4.2]
-  def change 
-    add_column :characters, :actor_id, :integer 
-  end 
+  def change
+    add_column :characters, :actor_id, :integer
+  end
 end
