b1fe2722bf1b5434b9a87e21bb79bd11deb94ee8
Sav Younts <savyounts@gmail.com>
2018-08-27 20:11:11 +0000

    automatically backed up by learn

--- db/migrate/007_add_name_and_genre.rb
+++ db/migrate/007_add_name_and_genre.rb
@@ -1,6 +1,5 @@
-class AddNameAndGenre <ActiveRecord::Migration[4.2]
+class AddGenre <ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :name, :string
     add_column :shows, :genre, :string
   end
 end
