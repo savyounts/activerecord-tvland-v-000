0ce1443146fcd145e3854327bf91a3f9967475ab
Sav Younts <savyounts@gmail.com>
2018-08-27 19:57:54 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,5 @@
-class CreateActors < ActiveRecord::Migration 
-  def change 
+class CreateActors < ActiveRecord::Migration
+  def change
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
