b1d66a29023fc8677866acd9c19a78744e3d53b7
Sav Younts <savyounts@gmail.com>
2018-08-27 20:22:57 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,6 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
   def say_that_thing_you_say
-    puts "#{self.name} says: #{self.catchphrase}"
+    "#{self.name} says: #{self.catchphrase}"
   end
 end
